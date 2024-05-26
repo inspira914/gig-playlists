2a909ce81211eaed33088a62143496991417c3ac
assassinrema <tacoassassin148@gmail.com>
2024-05-26 12:45:16 +0100

    add spotify upcoming playlist logic

---
+++ functions/upcoming_playlist/__init__.py
@@ -0,0 +1,1 @@
+from .upcoming_playlist_client import UpcomingPlaylistClient
---
+++ tests/test_upcoming_playlist_client.py
@@ -0,0 +1,110 @@
+from unittest.mock import Mock
+import json
+import pytest
+
+import spotipy
+
+from functions.upcoming_playlist import UpcomingPlaylistClient
+
+TEST_DATA_DIR = "resources/test_data"
+USER_ID = "12345"
+PLAYLIST_ID = "6789"
+ARTIST_NOT_IN_PLAYLIST = "1HxXNvsraqrsgfmju1yKk8"
+ARTIST_WITH_ONE_SONG = "32kCEAvtuFzIZA15zrhQDW"
+ARTIST_WITH_TEN_SONGS = "6FBDaR13swtiWwGhX1WQsP"
+
+
+@pytest.fixture
+def playlist_contents():
+    with open(f"{TEST_DATA_DIR}/playlist_contents/all.json", "r") as f:
+        playlist_contents = json.load(f)
+    return playlist_contents
+
+
+@pytest.fixture
+def sp(playlist_contents):
+    sp = Mock(spotipy.Spotify)
+    sp.me.return_value = {"id": USER_ID}
+    sp.user_playlist_create.return_value = {"id": PLAYLIST_ID}
+    sp.playlist_items.return_value = playlist_contents
+    return sp
+
+
+@pytest.fixture()
+def sp_top_tracks(sp, artist_id):
+    with open(f"{TEST_DATA_DIR}/top_tracks/{artist_id}.json", "r") as f:
+        top_tracks = json.load(f)
+    sp.artist_top_tracks.return_value = top_tracks
+    return sp
+
+
+@pytest.fixture()
+def top_track_uris(artist_id):
+    with open(f"{TEST_DATA_DIR}/top_tracks/uris/{artist_id}.json", "r") as f:
+        top_track_uris = json.load(f)["tracks"]
+    return top_track_uris
+
+
+@pytest.fixture()
+def tracks_in_playlist(artist_id):
+    with open(f"{TEST_DATA_DIR}/playlist_contents/{artist_id}.json", "r") as f:
+        tracks_in_playlist = json.load(f)["tracks"]
+    return tracks_in_playlist
+
+
+class TestAddArtist:
+    @pytest.mark.parametrize("artist_id", [ARTIST_NOT_IN_PLAYLIST, ARTIST_WITH_ONE_SONG])
+    def test_tracks_added_to_new_playlist(self, artist_id, sp_top_tracks, top_track_uris):
+        client = UpcomingPlaylistClient(sp_top_tracks)
+        client.add_artist(artist_id)
+        sp_top_tracks.user_playlist_create.assert_called_with(
+            USER_ID,
+            "upcoming gigs",
+            public=False,
+            collaborative=False
+        )
+        sp_top_tracks.playlist_add_items.assert_called_with(
+            playlist_id=PLAYLIST_ID,
+            items=top_track_uris
+        )
+
+    @pytest.mark.parametrize("artist_id", [ARTIST_NOT_IN_PLAYLIST, ARTIST_WITH_ONE_SONG])
+    def test_tracks_added_to_existing_playlist(self, artist_id, sp_top_tracks, top_track_uris):
+        client = UpcomingPlaylistClient(sp_top_tracks, PLAYLIST_ID)
+        client.add_artist(artist_id)
+        sp_top_tracks.playlist_add_items.assert_called_with(
+            playlist_id=PLAYLIST_ID,
+            items=top_track_uris
+        )
+        sp_top_tracks.user_playlist_create.assert_not_called()
+
+    def test_artist_has_songs_in_playlist_above_threshold(self, sp):
+        client = UpcomingPlaylistClient(sp, PLAYLIST_ID)
+        client.add_artist(ARTIST_WITH_TEN_SONGS)
+        sp.user_playlist_create.assert_not_called()
+        sp.playlist_add_items.assert_not_called()
+
+    def test_tracks_not_found_for_artist(self):
+        # TODO: add test for artist/tracks not found
+        pass
+
+
+class TestRemoveArtist:
+    def test_playlist_not_created(self, sp):
+        client = UpcomingPlaylistClient(sp)
+        client.remove_artist(ARTIST_WITH_TEN_SONGS)
+        sp.playlist_remove_all_occurrences_of_items.assert_not_called()
+
+    def test_artist_not_in_playlist(self, sp):
+        client = UpcomingPlaylistClient(sp, PLAYLIST_ID)
+        client.remove_artist(ARTIST_NOT_IN_PLAYLIST)
+        sp.playlist_remove_all_occurrences_of_items.assert_not_called()
+
+    @pytest.mark.parametrize("artist_id", [ARTIST_WITH_TEN_SONGS, ARTIST_WITH_ONE_SONG])
+    def test_artist_in_playlist(self, artist_id, sp, tracks_in_playlist):
+        client = UpcomingPlaylistClient(sp, PLAYLIST_ID)
+        client.remove_artist(artist_id)
+        sp.playlist_remove_all_occurrences_of_items.assert_called_with(
+            playlist_id=PLAYLIST_ID,
+            items=tracks_in_playlist
+        )
