5b6d5307673da7a2fb71669229037f6e8db6c50e
assassinrema <tacoassassin148@gmail.com>
2024-07-14 16:37:17 +0100

    add test of future gig

--- tests/integration/test_add_gig_to_upcoming_playlist.py
+++ tests/integration/test_add_gig_to_upcoming_playlist.py
@@ -1,53 +1,71 @@
 import logging
 import json
+from datetime import date, timedelta
 
 import boto3
 import pytest
-from botocore.exceptions import ClientError
 
 
 logger = logging.getLogger()
 EVENT_DIR = "resources/test_data/dynamodb_stream_events"
 TEST_USER = "USER#ed9af2a7-60e5-448f-8081-9c49966d7f15"
+TEST_ARTIST = "7oPftvlwr6VrsViSDV7fJY"
 
 
-class TestApiGateway:
+@pytest.fixture(scope="session")
+def lambda_arn():
+    lambda_key = "AddGigToUpcomingPlaylistFunctionArn"
+    client = boto3.client("cloudformation")
+    response = client.describe_stacks(StackName="gig-playlists")
 
-    @pytest.fixture(scope="session")
-    def lambda_arn(self):
-        lambda_key = "AddGigToUpcomingPlaylistFunctionArn"
-        client = boto3.client("cloudformation")
-        response = client.describe_stacks(StackName="gig-playlists")
+    stacks = response["Stacks"]
+    stack_outputs = stacks[0]["Outputs"]
+    api_outputs = [output for output in stack_outputs if output["OutputKey"] == lambda_key]
 
-        stacks = response["Stacks"]
-        stack_outputs = stacks[0]["Outputs"]
-        api_outputs = [output for output in stack_outputs if output["OutputKey"] == lambda_key]
+    if not api_outputs:
+        raise KeyError(f"{lambda_key} not found in stack gig-playlists")
 
-        if not api_outputs:
-            raise KeyError(f"{lambda_key} not found in stack gig-playlists")
+    return api_outputs[0]["OutputValue"]
 
-        return api_outputs[0]["OutputValue"]
 
-    def test_add_past_gig(self, lambda_arn):
-        with open(f"{EVENT_DIR}/create_gig.json") as f:
-            event = json.load(f)
-        dynamodb_stream = {"Records": [event]}
+@pytest.fixture(scope="session")
+def lambda_client():
+    return boto3.client("lambda")
 
-        try:
-            client = boto3.client("lambda")
-            response = client.invoke(
-                FunctionName=lambda_arn,
-                Payload=json.dumps(dynamodb_stream)
-            )
-        except ClientError:
-            logger.exception("Couldn't invoke function %s.", lambda_arn)
-            raise
 
-        assert response["StatusCode"] == 200
-        payload = json.load(response["Payload"])
-        print(payload)
-        print(type(payload))
-        assert len(payload) == 1
-        assert TEST_USER in payload.keys()
-        assert len(payload[TEST_USER]) == 0
-        # no deletion scheduled
+def test_add_future_gig(lambda_arn, lambda_client):
+    with open(f"{EVENT_DIR}/create_gig.json") as f:
+        event = json.load(f)
+    event["dynamodb"]["NewImage"]["date"]["S"] = (date.today() + timedelta(days=2)).strftime("%Y-%m-%d")
+    dynamodb_stream = {"Records": [event]}
+
+    response = lambda_client.invoke(
+        FunctionName=lambda_arn,
+        Payload=json.dumps(dynamodb_stream)
+    )
+
+    assert response["StatusCode"] == 200
+    payload = json.load(response["Payload"])
+    assert len(payload) == 1
+    assert TEST_USER in payload.keys()
+    assert len(payload[TEST_USER]) == 1
+    assert TEST_ARTIST == payload[TEST_USER][0]
+
+    # TODO: delete schedules
+
+
+def test_add_past_gig(lambda_arn, lambda_client):
+    with open(f"{EVENT_DIR}/create_gig.json") as f:
+        event = json.load(f)
+    dynamodb_stream = {"Records": [event]}
+
+    response = lambda_client.invoke(
+        FunctionName=lambda_arn,
+        Payload=json.dumps(dynamodb_stream)
+    )
+
+    assert response["StatusCode"] == 200
+    payload = json.load(response["Payload"])
+    assert len(payload) == 1
+    assert TEST_USER in payload.keys()
+    assert len(payload[TEST_USER]) == 0
