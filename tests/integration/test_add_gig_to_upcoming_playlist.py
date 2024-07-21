91f0ba42c06453851b66af2f59a8fc26b5438b59
assassinrema <tacoassassin148@gmail.com>
2024-07-21 15:19:05 +0100

    add test of schedule creation

--- tests/integration/test_add_gig_to_upcoming_playlist.py
+++ tests/integration/test_add_gig_to_upcoming_playlist.py
@@ -4,12 +4,14 @@ from datetime import date, timedelta
 
 import boto3
 import pytest
-
+from botocore.exceptions import ClientError
 
 logger = logging.getLogger()
 EVENT_DIR = "resources/test_data/dynamodb_stream_events"
 TEST_USER = "USER#ed9af2a7-60e5-448f-8081-9c49966d7f15"
 TEST_ARTIST = "7oPftvlwr6VrsViSDV7fJY"
+TEST_PLAYLIST = "70ZBZd7PC0g8f2gFg3kKGC"
+SCHEDULE_NAME = f"delete_{TEST_ARTIST}_from_{TEST_PLAYLIST}"
 
 
 @pytest.fixture(scope="session")
@@ -33,7 +35,12 @@ def lambda_client():
     return boto3.client("lambda")
 
 
-def test_add_future_gig(lambda_arn, lambda_client):
+@pytest.fixture
+def scheduler():
+    yield boto3.client("scheduler")
+
+
+def test_add_future_gig(lambda_arn, lambda_client, scheduler):
     with open(f"{EVENT_DIR}/create_gig.json") as f:
         event = json.load(f)
     event["dynamodb"]["NewImage"]["date"]["S"] = (date.today() + timedelta(days=2)).strftime("%Y-%m-%d")
@@ -51,10 +58,16 @@ def test_add_future_gig(lambda_arn, lambda_client):
     assert len(payload[TEST_USER]) == 1
     assert TEST_ARTIST == payload[TEST_USER][0]
 
-    # TODO: delete schedules
+    try:
+        scheduler.get_schedule(Name=SCHEDULE_NAME)
+    except ClientError:
+        pytest.fail("Delete schedule was not created")
+    scheduler.delete_schedule(Name=SCHEDULE_NAME)
+
+    # TODO: remove artist
 
 
-def test_add_past_gig(lambda_arn, lambda_client):
+def test_add_past_gig(lambda_arn, lambda_client, scheduler):
     with open(f"{EVENT_DIR}/create_gig.json") as f:
         event = json.load(f)
     dynamodb_stream = {"Records": [event]}
@@ -69,3 +82,6 @@ def test_add_past_gig(lambda_arn, lambda_client):
     assert len(payload) == 1
     assert TEST_USER in payload.keys()
     assert len(payload[TEST_USER]) == 0
+
+    with pytest.raises(ClientError):
+        scheduler.get_schedule(Name=SCHEDULE_NAME)
