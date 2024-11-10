2e3edf8697322a042dfe6b2f0610f15e59593a4a
assassinrema <tacoassassin148@gmail.com>
2024-11-10 21:28:29 +0000

    add angular material

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,10 +2,12 @@ import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import routeConfig from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
-    provideRouter(routeConfig)
+    provideRouter(routeConfig),
+    provideAnimationsAsync()
   ]
 };
