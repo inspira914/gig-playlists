c205d51a4690964c3f4b5003c30f74518cd3c990
assassinrema <tacoassassin148@gmail.com>
2024-11-23 11:27:51 +0000

    add datepicker

--- frontend/src/app/gig-form/gig-form.component.ts
+++ frontend/src/app/gig-form/gig-form.component.ts
@@ -1,28 +1,39 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { MatLuxonDateModule } from '@angular/material-luxon-adapter';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { DateTime } from 'luxon';
+
 import { GigService } from '../gig.service';
 
 @Component({
   standalone: true,
   selector: 'gig-form',
   templateUrl: './gig-form.component.html',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule,
+    MatButtonModule, MatDatepickerModule, MatLuxonDateModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GigFormComponent {
   gigService = inject(GigService);
   gigForm = new FormGroup({
     artist: new FormControl('', Validators.required),
-    venue: new FormControl(''),
-    spotifyArtistId: new FormControl(''),
-    date: new FormControl(''),
+    venue: new FormControl('', Validators.required),
+    spotifyArtistId: new FormControl('', Validators.required),
+    date: new FormControl<DateTime | null>(null, Validators.required),
   });
 
   onSubmit() {
+    // is this the best way to do this?
     this.gigService.addGig(
       this.gigForm.value.artist ?? '',
       this.gigForm.value.venue ?? '',
       this.gigForm.value.spotifyArtistId ?? '',
-      this.gigForm.value.date ?? '',
+      this.gigForm.value.date ?? DateTime.now(),
     );
   }
 }
--- frontend/src/app/gig.service.ts
+++ frontend/src/app/gig.service.ts
@@ -2,25 +2,27 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Gig } from './gig';
 import { environment } from './config';
+import { DateTime } from "luxon";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GigService {
-  // TODO: Retrieve this URL?
+  // TODO: change this URL to domain
   url = 'https://cjrpgycyf4.execute-api.eu-west-2.amazonaws.com/Prod/gigs';
 
   constructor(private http: HttpClient) {};
 
-  addGig(artist: string, venue: string, spotifyArtistId: string, date: string): void {
+  addGig(artist: string, venue: string, spotifyArtistId: string, date: DateTime): void {
+
     let gig: Gig = {
       artist: artist,
       userId: 'USER#e60d3adf-1bd5-4b5e-b71c-42582ed86bd6',
-      date: date,
+      date: date.toFormat("yyyy-MM-dd"),
       spotifyArtistId: spotifyArtistId,
       venue: venue,
     };
-    console.log(gig);
+
     this.http.post<Gig>(this.url, gig, {
       headers: {
         'Authorization': environment.token
