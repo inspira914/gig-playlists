aee5ac3b55b324fb241e0c1cc0547d3e271e4f55
assassinrema <tacoassassin148@gmail.com>
2025-03-16 16:02:38 +0000

    add login and signup pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,21 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component'
 import { GigFormComponent } from './gig-form/gig-form.component'
+import {LoginComponent} from "./login/login.component";
+import {SignupComponent} from "./signup/signup.component";
 
 const routeConfig: Routes = [
   {
     path: 'add-gig',
     component: GigFormComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
   },
   {
     path: '',
