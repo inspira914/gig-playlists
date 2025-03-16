47c630fa6fc4a662952387bfd2e579d5ab1a9187
assassinrema <tacoassassin148@gmail.com>
2025-03-16 11:26:57 +0000

    unit test lambda authoriser

--- functions/gigs_db_auth/app.py
+++ functions/gigs_db_auth/app.py
@@ -3,19 +3,14 @@ import boto3
 
 from aws_lambda_powertools.utilities.data_classes import event_source
 from aws_lambda_powertools.utilities.data_classes.api_gateway_authorizer_event import (
-    APIGatewayAuthorizerTokenEvent,
-    APIGatewayAuthorizerResponse,
+    APIGatewayAuthorizerTokenEvent, APIGatewayAuthorizerResponse, APIGatewayRouteArn,
 )
 
 
 ssm_client = boto3.client("ssm")
 
-
-@event_source(data_class=APIGatewayAuthorizerTokenEvent)
-def lambda_handler(event: APIGatewayAuthorizerTokenEvent, context):
-    arn = event.parsed_arn
-
-    policy = APIGatewayAuthorizerResponse(
+def create_policy(arn: APIGatewayRouteArn) -> APIGatewayAuthorizerResponse:
+    return APIGatewayAuthorizerResponse(
         principal_id="user",
         region=arn.region,
         aws_account_id=arn.aws_account_id,
@@ -23,9 +18,20 @@ def lambda_handler(event: APIGatewayAuthorizerTokenEvent, context):
         stage=arn.stage
     )
 
-    expected_token = ssm_client.get_parameter(Name="/gigs/crud_api_token")["Parameter"]["Value"]
-    if event.authorization_token == expected_token:
+
+def allow_access_for_valid_token(
+        policy: APIGatewayAuthorizerResponse,
+        provided_token: str,
+        expected_token: str) -> None:
+    if provided_token == expected_token:
         policy.allow_all_routes()
     else:
         policy.deny_all_routes()
+
+
+@event_source(data_class=APIGatewayAuthorizerTokenEvent)
+def lambda_handler(event: APIGatewayAuthorizerTokenEvent, context):
+    expected_token = ssm_client.get_parameter(Name="/gigs/crud_api_token")["Parameter"]["Value"]
+    policy = create_policy(event.parsed_arn)
+    allow_access_for_valid_token(policy, event.authorization_token, expected_token)
     return policy.asdict()
