cb22a574f4d50c464d72abed4bd0791b72336707
assassinrema <tacoassassin148@gmail.com>
2025-05-05 11:43:29 +0100

    get gigs from list

--- frontend/src/app/pages/gigs/gigs.component.ts
+++ frontend/src/app/pages/gigs/gigs.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import {MatToolbar} from "@angular/material/toolbar";
-import {MatList, MatListItem} from "@angular/material/list";
 import {MatIcon} from "@angular/material/icon";
 import {MatIconButton} from "@angular/material/button";
-import {MatLine} from "@angular/material/core";
 import {MatCard} from "@angular/material/card";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-gigs',
@@ -13,11 +12,17 @@ import {MatCard} from "@angular/material/card";
     MatIcon,
     MatIconButton,
     MatToolbar,
-    MatCard
+    MatCard,
+    NgForOf
   ],
   templateUrl: './gigs.component.html',
   styleUrl: './gigs.component.css'
 })
 export class GigsComponent {
-
+  gigs = [
+    { day: 'Mon', date: 30, month: 'December', title: 'blink-182 *' },
+    { day: 'Sun', date: 31, month: 'December', title: 'Green Day *' },
+    { day: 'Tue', date: 1, month: 'November', title: 'Everything Everything *' },
+    { day: 'Wed', date: 2, month: 'November', title: 'The World is a Beautiful Place and I Am No Longer Afraid to Die *' },
+  ];
 }
