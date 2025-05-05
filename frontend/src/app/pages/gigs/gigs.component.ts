f811ce9175343a424e9b7bb0d9497ef6b11e10ed
assassinrema <tacoassassin148@gmail.com>
2025-05-05 11:30:31 +0100

    basic gig list mockup

--- frontend/src/app/pages/gigs/gigs.component.ts
+++ frontend/src/app/pages/gigs/gigs.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatList, MatListItem} from "@angular/material/list";
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
+import {MatLine} from "@angular/material/core";
+import {MatCard} from "@angular/material/card";
 
 @Component({
   selector: 'app-gigs',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon,
+    MatIconButton,
+    MatToolbar,
+    MatCard
+  ],
   templateUrl: './gigs.component.html',
   styleUrl: './gigs.component.css'
 })
