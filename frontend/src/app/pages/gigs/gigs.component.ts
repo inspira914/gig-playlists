35eec07d95939283c1a4ee09d4b0eb7040847820
assassinrema <tacoassassin148@gmail.com>
2025-05-05 15:25:22 +0100

    split list by month

--- frontend/src/app/pages/gigs/gigs.component.ts
+++ frontend/src/app/pages/gigs/gigs.component.ts
@@ -5,6 +5,13 @@ import {MatIconButton} from "@angular/material/button";
 import {MatCard} from "@angular/material/card";
 import {NgForOf} from "@angular/common";
 
+interface Gig {
+  day: string;
+  date: number;
+  month: string;
+  title: string;
+}
+
 @Component({
   selector: 'app-gigs',
   standalone: true,
@@ -19,10 +26,32 @@ import {NgForOf} from "@angular/common";
   styleUrl: './gigs.component.css'
 })
 export class GigsComponent {
-  gigs = [
+  gigs: Gig[] = [
     { day: 'Mon', date: 30, month: 'December', title: 'blink-182 *' },
     { day: 'Sun', date: 31, month: 'December', title: 'Green Day *' },
     { day: 'Tue', date: 1, month: 'November', title: 'Everything Everything *' },
     { day: 'Wed', date: 2, month: 'November', title: 'The World is a Beautiful Place and I Am No Longer Afraid to Die *' },
   ];
+
+  gigsByMonth: { month: string; gigs: Gig[] }[] = [];
+
+  ngOnInit(): void {
+    this.gigsByMonth = this.groupByMonth(this.gigs);
+  }
+
+  groupByMonth(gigs: Gig[]): { month: string; gigs: Gig[] }[] {
+    const groups: { [month: string]: Gig[]} = {};
+
+    for (const gig of gigs) {
+      if (!groups[gig.month]) {
+        groups[gig.month] = [];
+      }
+      groups[gig.month].push(gig);
+    }
+
+    return Object.keys(groups).map(month => ({
+      month,
+      gigs: groups[month]
+    }));
+  }
 }
